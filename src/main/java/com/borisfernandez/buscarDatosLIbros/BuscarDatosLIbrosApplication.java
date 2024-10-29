52687a4e8d231d0ce87bd662cc3864faf984f0f3
Boris <164273833+borisfernande@users.noreply.github.com>
2024-10-28 20:20:19 -0500

    Subir archivos

---
+++ src/main/java/com/borisfernandez/buscarDatosLIbros/BuscarDatosLIbrosApplication.java
@@ -0,0 +1,13 @@
+package com.borisfernandez.buscarDatosLIbros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscarDatosLIbrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscarDatosLIbrosApplication.class, args);
+	}
+
+}
